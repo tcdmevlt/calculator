674cbc0becf0b124d7f198af68ba161b7d5a3b83
Violet <theycalledmevioled@gmail.com>
2024-02-29 20:22:10 +0200

    calculator with basic operations

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,17 @@
 public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public double divide(int a, int b) {
+        if (b == 0) {
+            return Double.POSITIVE_INFINITY;
+        }
+        return (double) a / b;
+    }
 }
